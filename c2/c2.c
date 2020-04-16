dc3735d4a1447def4a39857c8008f6b0cd024335
fulltimelink <fulltimelink@aliyun.com>
2020-04-16 22:23:25 +0800

    c2121

--- c2/c2.c
+++ c2/c2.c
@@ -3,6 +3,16 @@
 //
 #include <stdio.h>
 #include "c2.h"
+
+
+void c2121(){
+    printf("Gustav Mahler\n");
+    printf("Gustav\nMahler\n");
+    printf("Gustav ");
+    printf("Mahler\n");
+}
+
+
 void c2(){
-    printf("c2");
+    c2121();
 }
